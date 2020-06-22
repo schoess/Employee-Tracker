40f245cbe6c5f3c86a061f5ee5bc506c9bb8c3d8
Nick Schoess <schoessnick@gmail.com>
2020-06-22 16:09:42 -0500

    built basic options objects

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
-    password: "",
+    password: "skianddestroy",
     database: "work_db"
 });
 
@@ -19,3 +19,98 @@ connection.connect(function (err) {
     console.log(`Welcome to the easiest way to spy on your employees.\n`);
     start();
 });
+
+
+const optionsPrompt = 
+    {
+        type: "list",
+        message: "Would you like to view or add?",
+        name: "option",
+        choices: [
+            {
+                name: "View Departments, Employees and Roles",
+                value: "view",
+                short: "View"
+            },
+
+            {
+                name: "Add Departments, Employees and Roles",
+                value: "add",
+                short: "Add"
+            },
+
+            {
+                name: "Update the employee roles",
+                value: "update",
+                short: "Update"
+            },
+            {
+                name: "Exit Application",
+                value: "exit",
+                short: "Exit"
+            }
+        ]
+    };
+
+    const viewOptionsPrompt = 
+    {
+        type: "list",
+        message: "What would you like to view?",
+        name: "viewOption",
+        choices: [
+            {
+                name: "View Departments",
+                value: "vDepartment",
+                short: "ViewDepartment"
+            },
+
+            {
+                name: "View Employees",
+                value: "vEmployees",
+                short: "ViewEmployees"
+            },
+
+            {
+                name: "View Roles",
+                value: "vRoles",
+                short: "ViewRoles"
+            },
+            {
+                name: "Exit Application",
+                value: "exit",
+                short: "Exit"
+            }
+        ]
+    };
+
+    const addOptionsPrompt = 
+    {
+        type: "list",
+        message: "What would you like to Add?",
+        name: "addOption",
+        choices: [
+            {
+                name: "Add Departments",
+                value: "aDepartment",
+                short: "AddDepartment"
+            },
+
+            {
+                name: "Add Employees",
+                value: "aEmployees",
+                short: "AddEmployees"
+            },
+
+            {
+                name: "Add Roles",
+                value: "aRoles",
+                short: "AddRoles"
+            },
+            {
+                name: "Exit Application",
+                value: "exit",
+                short: "Exit"
+            }
+        ]
+    };
+
