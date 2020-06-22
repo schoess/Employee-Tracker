a7dd8bc3a58be1b339f48b668491ea279e8ceca0
Nick Schoess <schoessnick@gmail.com>
2020-06-22 15:58:32 -0500

    removed info

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
-    password: "skianddestroy",
+    password: "",
     database: "work_db"
 });
 
